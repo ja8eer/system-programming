f57a80b454314267cb8531d68713c038dc664347
ja8eer <57190486+ja8eer@users.noreply.github.com>
2019-11-12 19:46:45 -0700

    Add files via upload

--- setuid/setuid.c
+++ setuid/setuid.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pwd.h>
 #include<unistd.h>
 int main()
 {
-	printf("uid	:%d\n",getuid());
-	if(setuid(1000)!=0)
-		printf("cannot\n");
-	else 
-		printf("uid	:%d\n",getuid());
-	if(seteuid(0)!=0)
-		printf("yssss\n");
-	else
-		printf("eudi	:%d\n",geteuid());
-	if(setgid(0)!=0)
-		printf("lol\n");
+	struct passwd *pwd;
+	if((pwd=getpwuid(getuid()))!=NULL)
+	{
+		printf("username :%s\n",pwd->pw_name);
+		printf("dir :%s\n",pwd->pw_dir);
+		printf("shell :%s\n",pwd->pw_shell);
+	//	printf("username :%s\n",pwd->pw_name);
+	}
 
 }
-
